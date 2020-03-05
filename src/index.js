c78f252f6b659c896a2048534ebadca37d301bd1
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-05 20:15:02 -0300

    status bar styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
 const App = () => {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#141419" />
+      <Routes />
+    </>
+  );
 };
 
 export default App;
