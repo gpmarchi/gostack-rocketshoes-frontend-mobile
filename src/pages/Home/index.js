6b9a21c4ae3bdfcdf196c6bfee677adbad565fff
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-06 18:48:38 -0300

    home page styling

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { Text, Button } from 'react-native';
 import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { FlatList } from 'react-native';
 
-import { Container } from './styles';
+import {
+  Container,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  CartAmount,
+  CartAmountText,
+  AddButtonText,
+} from './styles';
 
 export default function Home({ navigation }) {
+  // onPress={() => navigation.navigate('Cart')}
   return (
     <Container>
-      <Text>Home</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Cart')}
-      />
+      <Product>
+        <ProductImage
+          source={{
+            uri:
+              'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+          }}
+        />
+        <ProductTitle>Tênis de Caminhada Leve Confortável</ProductTitle>
+        <ProductPrice>R$ 129,90</ProductPrice>
+        <AddButton onPress={() => navigation.navigate('Cart')}>
+          <CartAmount>
+            <Icon name="add-shopping-cart" size={30} color="#FFF" />
+            <CartAmountText>3</CartAmountText>
+          </CartAmount>
+          <AddButtonText>ADICIONAR</AddButtonText>
+        </AddButton>
+      </Product>
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,8 +1,58 @@
 import styled from 'styled-components/native';
+import { darken } from 'polished';
 
 export const Container = styled.View`
   background: #191920;
   flex: 1;
+`;
+
+export const Product = styled.View`
+  background: #fff;
+  border-radius: 4px;
+  padding: 10px;
+  width: 220px;
+`;
+
+export const ProductImage = styled.Image`
+  width: 200px;
+  height: 200px;
+`;
+
+export const ProductTitle = styled.Text`
+  color: #333;
+  font-size: 16px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-weight: bold;
+  font-size: 21px;
+  margin: 14px 0;
+`;
+
+export const AddButton = styled.TouchableOpacity`
+  background: #7159c1;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
+  border-radius: 4px;
+`;
+
+export const CartAmount = styled.View`
+  flex-direction: row;
+  padding: 12px;
+  align-items: center;
+  background: ${darken(0.03, '#7159c1')};
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+`;
+
+export const CartAmountText = styled.Text`
+  margin: 0 4px 0 10px;
+  color: #fff;
+`;
+
+export const AddButtonText = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  flex: 1;
+  text-align: center;
 `;
