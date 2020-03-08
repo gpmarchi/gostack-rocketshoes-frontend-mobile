b5df2b5c6e5397830e45867925edfd58e45a6d54
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-08 20:22:20 -0300

    redux configuration for cart state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 
 import './config/ReactotronConfig';
 
 import Routes from './routes';
+import store from './store';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#141419" />
       <Routes />
-    </>
+    </Provider>
   );
 };
 
