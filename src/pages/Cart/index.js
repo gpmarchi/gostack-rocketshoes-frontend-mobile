e0b39ebd4a80386fbb5d533e52ff6199e1506151
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-09 18:01:49 -0300

    listing cart products from redux state

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import {
   Container,
@@ -23,69 +25,48 @@ import {
   OrderText,
 } from './styles';
 
-export default function Cart() {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <Products>
-        <Product>
-          <ProductInfo>
-            <ProductImage
-              source={{
-                uri:
-                  'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
-              }}
-            />
-            <ProductDetails>
-              <ProductTitle>Tênis de Caminhada Leve Confortável</ProductTitle>
-              <ProductPrice>R$ 129,90</ProductPrice>
-            </ProductDetails>
-            <ProductDelete>
-              <Icon name="delete-forever" size={30} color="#7159c1" />
-            </ProductDelete>
-          </ProductInfo>
-          <ProductControls>
-            <AmountControls>
-              <AmountControlButton>
-                <Icon name="remove-circle-outline" size={25} color="#7159c1" />
-              </AmountControlButton>
-              <ProductAmount value={String(1)} />
-              <AmountControlButton>
-                <Icon name="add-circle-outline" size={25} color="#7159c1" />
-              </AmountControlButton>
-            </AmountControls>
-            <ProductSubtotal>R$ 539.65</ProductSubtotal>
-          </ProductControls>
-        </Product>
-
-        <Product>
-          <ProductInfo>
-            <ProductImage
-              source={{
-                uri:
-                  'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
-              }}
-            />
-            <ProductDetails>
-              <ProductTitle>Tênis de Caminhada Leve Confortável</ProductTitle>
-              <ProductPrice>R$ 129,90</ProductPrice>
-            </ProductDetails>
-            <ProductDelete>
-              <Icon name="delete-forever" size={30} color="#7159c1" />
-            </ProductDelete>
-          </ProductInfo>
-          <ProductControls>
-            <AmountControls>
-              <AmountControlButton>
-                <Icon name="remove-circle-outline" size={25} color="#7159c1" />
-              </AmountControlButton>
-              <ProductAmount value={String(1)} />
-              <AmountControlButton>
-                <Icon name="add-circle-outline" size={25} color="#7159c1" />
-              </AmountControlButton>
-            </AmountControls>
-            <ProductSubtotal>R$ 539.65</ProductSubtotal>
-          </ProductControls>
-        </Product>
+        {cart.map(product => (
+          <Product key={String(product.id)}>
+            <ProductInfo>
+              <ProductImage
+                source={{
+                  uri: product.image,
+                }}
+              />
+              <ProductDetails>
+                <ProductTitle>{product.title}</ProductTitle>
+                <ProductPrice>{product.formattedPrice}</ProductPrice>
+              </ProductDetails>
+              <ProductDelete
+                onPress={() =>
+                  dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                }
+              >
+                <Icon name="delete-forever" size={30} color="#7159c1" />
+              </ProductDelete>
+            </ProductInfo>
+            <ProductControls>
+              <AmountControls>
+                <AmountControlButton>
+                  <Icon
+                    name="remove-circle-outline"
+                    size={25}
+                    color="#7159c1"
+                  />
+                </AmountControlButton>
+                <ProductAmount value={String(product.amount)} />
+                <AmountControlButton>
+                  <Icon name="add-circle-outline" size={25} color="#7159c1" />
+                </AmountControlButton>
+              </AmountControls>
+              <ProductSubtotal>R$ 539.65</ProductSubtotal>
+            </ProductControls>
+          </Product>
+        ))}
         <CartTotal>
           <TotalText>TOTAL</TotalText>
           <Total>R$ 1678,89</Total>
@@ -97,3 +78,14 @@ export default function Cart() {
     </Container>
   );
 }
+
+Cart.propTypes = {
+  cart: PropTypes.shape().isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
