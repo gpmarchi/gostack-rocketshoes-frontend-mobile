731621ab9333541fa72b910ea6def37774bf1afb
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-03-09 16:14:22 -0300

    reactotron redux config

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,11 +1,13 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({host: '192.168.0.26'})
+  const tron = Reactotron.configure({ host: '192.168.0.26' })
     .useReactNative()
+    .use(reactotronRedux())
     .connect();
 
-  console.tron = tron;
-
   tron.clear();
+
+  console.tron = tron;
 }
